a94f94015667fbf8049a1fa3cea39d6306082ad4
David <david.van.steertegem@gmail.com>
2020-02-17 16:31:13 +0100

    added spinner because we can

--- app/src/main/java/be/ehb/tabpasbar/fragments/OrderFragment.java
+++ app/src/main/java/be/ehb/tabpasbar/fragments/OrderFragment.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
+import android.text.BoringLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import be.ehb.tabpasbar.R;
@@ -27,9 +30,10 @@ public class OrderFragment extends Fragment {
             String locationKey = getResources().getString(R.string.pref_location);
             String selectedLocation = prefs.getString(locationKey, "Dilbeek");
 
-            Toast.makeText(getActivity(), "Order from "+selectedLocation+" confirmed", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), foodSpinner.getSelectedItem()+ " from "+selectedLocation+" confirmed", Toast.LENGTH_LONG).show();
         }
     };
+    private Spinner foodSpinner;
 
 
     public OrderFragment() {
@@ -45,6 +49,12 @@ public class OrderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_order, container, false);
+
+        //extra voor David
+        foodSpinner = rootView.findViewById(R.id.sp_foods);
+        String[] foods = {"patatas bravas", "patatas stoutas", "calamares"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_item, foods);
+        foodSpinner.setAdapter(adapter);
 
         Button orderBtn = rootView.findViewById(R.id.btn_order);
         orderBtn.setOnClickListener(orderListener);
